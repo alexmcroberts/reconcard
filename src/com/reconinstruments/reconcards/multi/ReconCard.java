98f960eaf8a908293c9b203f9f3c5dbda21a2f35
alexmcroberts <alex@alexmcroberts.co.uk>
2013-06-18 16:14:19 -0700

    renamed to use .multi folder

---
+++ src/com/reconinstruments/reconcards/multi/ReconCard.java
@@ -0,0 +1,102 @@
+package com.reconinstruments.reconcards.multi;
+
+import java.io.File;
+import java.io.Serializable;
+
+import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+public class ReconCard implements Serializable {
+
+	/**
+	 * required for serialization
+	 */
+	private static final long serialVersionUID = -765887146556379608L;
+	private int nextCard, nextCardDelay, nextCardAnimationDuration;
+	private String nextCardAnimation;
+	private int id, leftCard, rightCard, upCard, downCard, selectCard, selectHoldCard, backCard;
+
+	private String imagePath;
+	private Resources image;
+	
+	public ReconCard(String image, int id, int left, int right, int up, int down, int select, int selectHold, int back, int nextCard, int nextCardDelay, String nextCardAnimation, int nextCardAnimationDuration) {
+		this.leftCard = left;
+		this.rightCard = right;
+
+		this.upCard = up;
+		this.downCard = down;
+		this.selectCard = select;
+		this.selectHoldCard = selectHold;
+		this.backCard = back;
+		this.imagePath = image;
+		this.id = id;
+		this.nextCard = nextCard;
+		this.nextCardDelay = nextCardDelay;
+		this.nextCardAnimation = nextCardAnimation;
+		this.nextCardAnimationDuration = nextCardAnimationDuration;
+	}
+	
+	public String imagePath() {
+		return this.imagePath;
+	}
+	
+	public Drawable getDrawable() throws Exception {
+		File file = new File(imagePath);
+		
+		if ( !file.exists() ) {
+			throw new Exception("file not found at " + imagePath);
+		}
+		
+		return new BitmapDrawable(image, imagePath);
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public int getLeftIndex() {
+		return leftCard;
+	}
+	
+	public int getRightIndex() {
+		return rightCard;
+	}
+	
+	public int getUpIndex() {
+		return upCard;
+	}
+	
+	public int getDownIndex() {
+		return downCard;
+	}
+
+	public int getBackIndex() {
+		return backCard;
+	}
+	
+	public int getSelectIndex() {
+		return selectCard;
+	}
+	
+	public int getSelectHoldIndex() {
+		return selectHoldCard;
+	}
+	
+	public int getNextCard() {
+		return this.nextCard;
+	}
+	
+	public int getNextCardDelay() {
+		return this.nextCardDelay;
+	}
+
+	public String getNextCardAnimation() {
+		return this.nextCardAnimation;
+	}
+	
+	public int getNextCardAnimationDuration() {
+		return this.nextCardAnimationDuration;
+	}
+	
+}
---
+++ src/com/reconinstruments/reconcards/multi/SingleApplicationActivity.java
@@ -0,0 +1,300 @@
+package com.reconinstruments.reconcards.multi;
+ 
+import com.reconinstruments.reconcards.multi.R;
+import com.reconinstruments.reconcards.multi.ReconCardApplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.view.Display;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+ 
+public class SingleApplicationActivity extends Activity{
+	public static final String TAG = "SingleListItemActivity";
+
+	private ImageView imageOne, imageTwo, tempImage;
+	private int currentCard;
+	private Handler cardHandler;
+	private NextCardRunnable mNextCardRunnable;
+	private boolean imageToggle = false;
+	private ReconCardApplication application;
+	
+	boolean flagShortPress = false;
+	boolean flagLongPress  = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.single_list_item_view);
+	}
+    
+    @Override
+	public void onResume() {		
+		cardHandler = new Handler();
+		
+		imageOne = (ImageView) findViewById(R.id.image_one);
+		imageTwo = (ImageView) findViewById(R.id.image_two);
+
+        Bundle extras = getIntent().getExtras();
+        Intent intentIncoming = getIntent();
+        if(extras != null) {
+            application = (ReconCardApplication) intentIncoming.getParcelableExtra("app");
+
+			setCard(1);
+		}
+		super.onResume();
+    }
+    
+    public void onPause() {
+		super.onPause();
+		cardHandler.removeCallbacks(mNextCardRunnable);
+	}
+    
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+		if (application.getCards().size() < 1) return false;
+		
+		Log.d(TAG,  "IN LONG PRESS");
+		
+		int nextCard = -1;
+		
+		try {
+			ReconCard thisCard = application.getCard(currentCard);
+	
+			switch(keyCode) {
+			case KeyEvent.KEYCODE_DPAD_CENTER:
+				nextCard = thisCard.getSelectHoldIndex();
+				break;
+			}
+	
+			if (nextCard > -1) {
+				Log.d(TAG, "next card: " + nextCard);
+				setCard(nextCard);
+			} else if (nextCard == -1){
+				finish();
+			}
+		}
+		catch ( Exception e ) {
+			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+		
+		return true;
+    }
+    
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (application.getCards().size() < 1) return false;
+		event.startTracking();
+		int nextCard = -1;
+		try {
+			ReconCard thisCard = application.getCard(currentCard);
+			
+			switch(keyCode) {
+			case KeyEvent.KEYCODE_DPAD_RIGHT:
+				nextCard = thisCard.getRightIndex();
+				break;
+				
+			case KeyEvent.KEYCODE_DPAD_LEFT:
+				nextCard = thisCard.getLeftIndex();
+				break;
+				
+			case KeyEvent.KEYCODE_DPAD_UP:
+				nextCard = thisCard.getUpIndex();
+				break;
+				
+			case KeyEvent.KEYCODE_DPAD_DOWN:
+				nextCard = thisCard.getDownIndex();
+				break;
+				
+			case KeyEvent.KEYCODE_BACK:
+				nextCard = thisCard.getBackIndex();
+				break;
+				
+			case KeyEvent.KEYCODE_DPAD_CENTER:
+				nextCard = thisCard.getSelectIndex();
+				break;
+			}
+			
+			if (nextCard > -1) {
+				Log.d(TAG, "next card: " + nextCard);
+				setCard(nextCard);
+			} else if (nextCard == -1){
+				finish();
+			}
+		}
+		catch ( Exception e ) {
+			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+		
+		return true;
+	}
+	
+	/* The only way to get width / height to build is with these supression warnings */
+	@SuppressWarnings("deprecation")
+	@SuppressLint("NewApi")
+	public void showImage(ImageView image, Drawable imageDrawable, String cardDirection, int cardAnimationDuration) {
+		int version = android.os.Build.VERSION.SDK_INT;
+		int width;
+		int height;
+	    Display display = getWindowManager().getDefaultDisplay(); 
+		if (version >= 13){
+		    Point size = new Point();
+		    display.getSize(size);
+		    width = size.x;
+		    height = size.y;
+		}
+		else {
+		    width = display.getWidth();
+		    height = display.getHeight();
+		}
+    	
+    	int xStart = 0;
+    	int yStart = 0;
+    	boolean animationRequested = true;
+    	
+    	if ( cardDirection.equals("up") ) {
+        	xStart = 0;
+        	yStart = height;
+        	Log.v(TAG, "up");
+    	}
+    	else if ( cardDirection.equals("down") ) {
+    		xStart = 0;
+    		yStart = 0 - height;
+    		Log.v(TAG, "down");
+    	}
+    	else if ( cardDirection.equals("left") ) {
+    		xStart = width;
+    		yStart = 0;
+    		Log.v(TAG, "left");
+    	}
+		else if ( cardDirection.equals("right") ) {
+			xStart = 0 - width;
+			yStart = 0;
+			Log.v(TAG, "right");
+		}
+		else if ( cardDirection.equals("fade-in") ) {
+			xStart = 0 - width;
+			yStart = 0;
+			Log.v(TAG, "fade-in");
+		}
+		else if ( cardDirection.equals("fade-out") ) {
+			xStart = 0 - width;
+			yStart = 0;
+			Log.v(TAG, "fade-out");
+		}
+		else {
+			Log.v(TAG, "no animation requested");
+			animationRequested = false;
+		}
+
+    	// populate the image
+    	image.setImageDrawable(imageDrawable);
+    	image.setVisibility(View.VISIBLE);
+
+    	// only animate if it's requested
+    	if ( animationRequested ) {
+    		Animation animation;
+    		animation = prepareAnimation(cardDirection, xStart, yStart);
+	    	
+	    	// define the transition
+	    	try {
+		    	animation.setDuration(cardAnimationDuration);  
+		        image.startAnimation(animation);
+	    	}
+	    	catch(IllegalArgumentException e) {
+	    		Toast.makeText(getApplicationContext(), "card animation duration must be > 0", Toast.LENGTH_LONG).show();
+	    	}
+    	}
+    }
+	
+	public Animation prepareAnimation(String cardDirection, int xStart, int yStart) {
+		if ( cardDirection.equals("fade-in") || cardDirection.equals("fade-out") ) {
+    		ScaleAnimation animation;
+
+    		if ( cardDirection.equals("fade-in") ) {
+	    		animation = new ScaleAnimation(xStart, 0, yStart, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+	    		//scale in
+    		}
+    		else {
+	    		//scale out
+	    		animation = new ScaleAnimation(0, xStart, 0, yStart, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+    		}
+
+    		return animation;
+    	}
+    	else {
+    		// sliding!
+	    	TranslateAnimation animation;
+	    	animation = new TranslateAnimation( Animation.RELATIVE_TO_PARENT, xStart, Animation.RELATIVE_TO_PARENT, 0, Animation.RELATIVE_TO_PARENT, yStart, Animation.RELATIVE_TO_PARENT, 0 );
+	    	
+			return animation;
+    	}
+	}
+    
+	public void setCard(int currentCard) {
+		this.currentCard = currentCard;
+		cardHandler.removeCallbacks(mNextCardRunnable);
+		try { 
+			ReconCard nextCard = application.getCard(currentCard);
+
+	    	// select which imageView will we use
+	    	tempImage = (imageToggle) ? imageOne : imageTwo;
+	    	
+	    	Log.v(TAG, "IMAGE " +tempImage.toString());
+	    	
+	    	// invert imageToggle for next time around
+	    	imageToggle = !imageToggle;
+	    	
+	    	Log.v(TAG, "IMAGETOGGLE " +imageToggle);
+	    	
+	    	try {
+	    		Drawable nextBitmap = nextCard.getDrawable();
+	    
+		    	// show image
+		    	this.showImage(tempImage, nextBitmap, nextCard.getNextCardAnimation(), nextCard.getNextCardAnimationDuration());
+				
+		    	// hide the previous image
+		    	tempImage = (imageToggle) ? imageOne : imageTwo;
+		    	tempImage.setVisibility(View.GONE);
+		    	
+		    	// if next="" an int, show it now!
+				if(nextCard.getNextCard() > -1) {
+					mNextCardRunnable = new NextCardRunnable(nextCard.getNextCard());
+					cardHandler.postDelayed(mNextCardRunnable, nextCard.getNextCardDelay());
+				}
+	    	}
+	    	catch ( Exception e ) {
+	    		Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+	    	}
+		}
+		catch ( Exception e ) {
+			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+	}
+	
+	class NextCardRunnable implements Runnable {
+
+		private int nextCard;
+		
+		public NextCardRunnable(int nextCard) {
+			this.nextCard = nextCard;
+		}
+		
+		@Override
+		public void run() {
+			Log.v(TAG, "RUNNING " +nextCard);
+			setCard(nextCard);
+		}
+	}
+	
+}
